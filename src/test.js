0f847f2f35a70eff9863b213c636b040c9300439
SuryaBatchu <surya.batchu@growisto.com>
2021-09-16 15:23:54 +0530

    done the things uptodate

--- src/test.js
+++
@@ -1,5 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-const firestore = firebase.firestore();
-firestore.collection('users').doc(KaC4cMD6DPTNJtOwgpZQ).collection('cartItems').doc(kS5IxFEFvkTXEtqTCNWb);
-firestore.doc('/users/KaC4cMD6DPTNJtOwgpZQ/cartItems/kS5IxFEFvkTXEtqTCNWb');
